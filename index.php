7010867a23f628ced3f3fcc1a4860eace8a062e2
Peter <p.timoshin2009@yandex.ru>
2024-03-04 00:23:42 +0300

    Intermediate commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<html>
+  <head>
+    <title>SRP Store</title>
+  </head>
+  <body>
+    <form action="/search.php">
+    <input type="text" id="query" name="query" placeholder="Search...">
+     <input type="submit" value="Submit">
+     <ul>
+       <li>App 1 <span> App 1 description</li>
+       <li>App 2</li>
+       <li>App 3</li>
+       <li>App 4</li>
+       <li>App 5</li>
+     </ul>
+    <form>
+  </body>
+</html>
+
