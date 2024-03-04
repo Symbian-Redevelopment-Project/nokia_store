1407448c63c5e7b5df838eec918c14db436fb997
Petrprogs <p.timoshin2009@yandex.ru>
2024-03-04 16:41:00 +0300

    Created template for index page. Created styles CSS file.

--- index.php
+++ index.php
@@ -1,19 +1,39 @@
+<!DOCTYPE html>
 <html>
-  <head>
-    <title>SRP Store</title>
-  </head>
-  <body>
-    <form action="/search.php">
-    <input type="text" id="query" name="query" placeholder="Search...">
-     <input type="submit" value="Submit">
-     <ul>
-       <li>App 1 <span> App 1 description</li>
-       <li>App 2</li>
-       <li>App 3</li>
-       <li>App 4</li>
-       <li>App 5</li>
-     </ul>
-    <form>
-  </body>
-</html>
+<head>
+  <title>SRP Store</title>
+  <link rel="stylesheet" type="text/css" href="styles.css" />
+</head>
 
+<body>
+  <form action="/search.php">
+    <input type="text" id="query" name="query" placeholder="Search...">
+    <input type="submit" value="Submit">
+</form>
+      <div class="widgets-group widgets-group_top bb0" id="sz_gallery_loader">
+        <div class="list-link__ava_2 p80"> <span> <img src="" aria-label="" alt="" class="preview  s81_80" width="80" height="80">
+          </span> </div>
+        <div class="cf"> <span class="grey right"> </span> <a href="Href_to_app1" class="arrow_link strong_link"><!-- --><b class="darkblue break-word">App name 1</b></a>
+          <div class="pad_t_a break-word">App description 1</div>
+          <div class="oh">
+            <div class="oh grey pad_t_a">
+              <span class="left">Symbian<span>
+                <span >Size in mb<span>
+            </div>
+          </div>
+        </div>
+        <div class="list-link__ava_2 p80"> <span> <img src="app_pic" aria-label="" alt="" class="    preview  s81_80" width="80" height="80">
+          </span> </div>
+        <div class="cf"> <span class="grey right"> </span> <a href="href_to_app2" class="arrow_link strong_link"><!-- --><b class="darkblue break-word">App name 2</b></a>
+          <div class="pad_t_a break-word">App description 2</div>
+          <div class="oh">
+            <div class="oh grey pad_t_a"> 
+              <span class="left">Java</span> 
+              <span class="t-padd_left">360x640</span>
+              <span>Size in mb</span>
+            </div>
+          </div>
+        </div>
+      </div>
+</body>
+</html>
