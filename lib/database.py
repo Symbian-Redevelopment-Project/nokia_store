a69fcf0e0b44a9ab6f525596722e220506108de1
Wunder Wungiel <dredlok706@gmail.com>
2024-03-08 21:32:24 +0100

    Searching & pagination

--- lib/database.py
+++ lib/database.py
@@ -13,6 +13,26 @@ conn = psycopg2.connect(database=config["DB_NAME"],
 class WrongCategoryError(Exception):
     pass
 
+def format_results(results_list):
+
+    results = {}
+    for result_list in results_list:
+        results[result_list[0]] = {
+            "id": result_list[0],
+            "title": result_list[1],
+            "file": result_list[2],
+            "category_name": get_category_name(result_list[3]),
+            "category_id": result_list[3],
+            "description": result_list[4].replace("postgres$#", "\n").strip(),
+            "publisher": result_list[5],
+            "version": result_list[6],
+            "platform": result_list[7],
+            "screenshots_count": result_list[8],
+            'img': result_list[9]
+        }
+    
+    return results
+
 def get_apps(categoryId=None):
 
     cursor = conn.cursor()
@@ -30,25 +50,9 @@ def get_apps(categoryId=None):
         cursor.execute("SELECT * FROM apps WHERE category=%s", (int(categoryId),))
     
     results_list = cursor.fetchall()
-
-    results = {}
-    for result_list in results_list:
-        results[result_list[0]] = {
-            "id": result_list[0],
-            "title": result_list[1],
-            "file": result_list[2],
-            "category_name": get_category_name(result_list[3]),
-            "category_id": result_list[3],
-            "description": result_list[4].replace("postgres$#", "\n").strip(),
-            "publisher": result_list[5],
-            "version": result_list[6],
-            "platform": result_list[7],
-            "screenshots_count": result_list[8],
-            'img': result_list[9]
-        }
-
     cursor.close()
-    return results
+
+    return format_results(results_list)
 
 
 def get_apps_categories():
@@ -75,3 +79,13 @@ def get_category_id(categoryName):
     result = cursor.fetchall()[0][0]
     cursor.close()
     return result
+
+def search(query):
+
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM apps WHERE LOWER(title) LIKE LOWER(%s)", ('%' + query + '%',))
+
+    results_list = cursor.fetchall()
+    cursor.close()
+
+    return format_results(results_list)
